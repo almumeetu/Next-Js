dd6fcbba6e7d50df7ad9dff30c70f964ee3b9d3c
Al Mumeetu Saikat <amsaikat@softzino.com>
2025-10-30 10:04:15 +0600

    update

--- project/app/about/page.tsx
+++ project/app/about/page.tsx
@@ -1,10 +1,8 @@
 
 export default function About() {
   return (
-     <main>
         <div className="max-w-4xl mx-auto py-16 px-4 sm:px-6 lg:px-8"> 
             <h1 className="text-4xl text-amber-300">Hello! This Is About Page</h1>
          </div>
-     </main>
   );
 }
--- project/app/service/page.tsx
+++ project/app/service/page.tsx
@@ -1,10 +1,8 @@
 
 export default function About() {
   return (
-     <main>
         <div className="max-w-4xl mx-auto py-16 px-4 sm:px-6 lg:px-8"> 
             <h1 className="text-4xl text-amber-700">Hello! This Is Service Page</h1>
          </div>
-     </main>
   );
 }
